3bd7a633850b882fde5394e68173c54dfdb9cd3a
johnfredy09 <john.gonzalez2@o365.unab.edu.co>
2021-10-17 21:34:33 -0500

    se incorpora js al login

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function inicioSesion(){
+    var usuario,contraseña;
+    var usuario=document.getElementById("usuario").value;
+    var contraseña=document.getElementById("contraseña").value;
+    
+    alert("Usted a inisiado Sesión correctametne");
+}
